c47ae13e17fd5a475495ef72ec264f7544b1b95c
Jake Strasler <straslerj@msoe.edu>
2024-04-24 11:49:51 -0500

    Updates the structure of the vector store to be more flexible

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import create_vector_store as cvs
 
 if __name__ == "__main__":
-    doc_chat = cvs.DocChat(model_id="gpt2")
+    doc_chat = cvs.DocChat(model_id="gpt2", mode="nfl")
     response = doc_chat.qNa(
         input("What would you like to know about the NFL rulebook?\n")
     )
